7829e6210609423294a1900cee6ed9cda033be4e
Cyrus Ramavarapu <tribunus.vii@gmail.com>
2021-12-27 21:18:51 -0500

    Finished tutorial part 3

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,15 @@
 import * as React from "react"
 import Layout from '../components/layout'
+import { StaticImage } from 'gatsby-plugin-image'
 
 const IndexPage = () => {
     return (
         <Layout pageTitle="Home Page">
             <p>Using a tutorial :3.</p>
+            <StaticImage
+                alt="Blahaj out and about"
+                src="../images/blahaj.jpg"
+            />
         </Layout>
     )
 }
